92f58a7c175217d0e75d4fe1895c1ef6b023d015
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2017-09-26 15:53:49 +0300

    Code formating. Removed unecessary escaping. Function definitions

--- classes/controller/blocks/class-p4bks-blocks-subheader-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-subheader-controller.php
@@ -7,10 +7,9 @@ if ( ! class_exists( 'P4BKS_Blocks_Subheader_Controller' ) ) {
 	class P4BKS_Blocks_Subheader_Controller extends P4BKS_Blocks_Controller {
 
 		/**
-		 *
+		 * function to load the block and define its name
 		 */
 		public function load() {
-			// --- Set here the name of your block ---
 			$this->block_name = 'subheader';
 			parent::load();
 		}
@@ -22,24 +21,22 @@ if ( ! class_exists( 'P4BKS_Blocks_Subheader_Controller' ) ) {
 		public function prepare_fields() {
 			$fields = array(
 				array(
-					'label'  => esc_html__( 'Title', 'planet4-blocks' ),
-					'attr'   => 'title',
-					'type'   => 'text',
+					'label' => __( 'Title', 'planet4-blocks' ),
+					'attr'  => 'title',
+					'type'  => 'text',
 				),
 				array(
-					'label'  => esc_html__( 'Description', 'planet4-blocks' ),
-					'attr'   => 'descr',
-					'type'   => 'textarea',
+					'label' => __( 'Description', 'planet4-blocks' ),
+					'attr'  => 'descr',
+					'type'  => 'textarea',
 				),
 			);
 
-			/*
-			 * Define the Shortcode UI arguments.
-			 */
+			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = array(
-				'label' => esc_html__( 'Subheader', 'planet4-blocks' ),
+				'label'         => __( 'Subheader', 'planet4-blocks' ),
 				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-plugin-blocks/icons/subheader.png' ) . '" />',
-				'attrs' => $fields,
+				'attrs'         => $fields,
 			);
 
 			shortcode_ui_register_for_shortcode( 'shortcake_' . $this->block_name, $shortcode_ui_args );
@@ -49,29 +46,27 @@ if ( ! class_exists( 'P4BKS_Blocks_Subheader_Controller' ) ) {
 		 * Callback for the shortcake_twocolumn shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array $fields
-		 * @param string $content
-		 * @param string $shortcode_tag
+		 * @param array $fields Array of fields that are to be used in the template.
+		 * @param string $content The content of the post.
+		 * @param string $shortcode_tag The shortcode tag (shortcake_blockname).
 		 *
-		 * @return string
+		 * @return string The complete html of the block
 		 */
-		public function prepare_template( $fields, $content, $shortcode_tag ) : string {
+		public function prepare_template( $fields, $content, $shortcode_tag ): string {
 
 			$fields = shortcode_atts( array(
-				'title'    	=> '',
-				'descr'     => '',
+				'title' => '',
+				'descr' => '',
 			), $fields, $shortcode_tag );
 
-			$fields['title'] 		= wp_kses_post( $fields['title']);
-			$fields['descr'] 		= wp_kses_post( $fields['descr']);
-
-			$data = ['fields' => $fields];
-
-
+			$data = [
+				'fields' => $fields
+			];
 
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
 			$this->view->block( $this->block_name, $data );
+
 			return ob_get_clean();
 		}
 	}
