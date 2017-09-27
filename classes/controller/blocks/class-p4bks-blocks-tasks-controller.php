ca3276a251272cae98bb84632361eb82aadb94d5
Kiriakos Diamantis <replicator31@hotmail.com>
2017-09-27 12:21:27 +0300

    Fixed codesniffer inspection warnings based on on wp standards

--- classes/controller/blocks/class-p4bks-blocks-tasks-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-tasks-controller.php
@@ -4,6 +4,11 @@ namespace P4BKS\Controllers\Blocks;
 
 if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 
+	/**
+	 * Class P4BKS_Blocks_Tasks_Controller
+	 *
+	 * @package P4BKS\Controllers\Blocks
+	 */
 	class P4BKS_Blocks_Tasks_Controller extends P4BKS_Blocks_Controller {
 
 		/**
@@ -26,10 +31,10 @@ if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 		 */
 		public function prepare_fields() {
 
-			// This block will have 4 different columns with same fields
+			// This block will have 4 different columns with same fields.
 			$fields = [];
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$ordinal = $i . date( "S", mktime( 0, 0, 0, 0, $i, 0 ) );
+				$ordinal = $i . date( 'S', mktime( 0, 0, 0, 0, $i, 0 ) );
 				$field   = [
 					[
 						'label' => esc_html__( 'Title', 'planet4-blocks' ),
@@ -53,13 +58,7 @@ if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 						'label'       => esc_html__( 'Attachment', 'shortcode-ui' ),
 						'attr'        => 'attachment_' . $i,
 						'type'        => 'attachment',
-						/*
-						 * These arguments are passed to the instantiation of the media library:
-						 * 'libraryType' - Type of media to make available.
-						 * 'addButton'   - Text for the button to open media library.
-						 * 'frameTitle'  - Title for the modal UI once the library is open.
-						 */
-						'libraryType' => array( 'image' ),
+						'libraryType' => [ 'image' ],
 						'addButton'   => esc_html__( 'Select Image', 'shortcode-ui' ),
 						'frameTitle'  => esc_html__( 'Select Image', 'shortcode-ui' ),
 					],
@@ -89,22 +88,8 @@ if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 			 * Define the Shortcode UI arguments.
 			 */
 			$shortcode_ui_args = array(
-				/*
-				 * How the shortcode should be labeled in the UI. Required argument.
-				 */
 				'label'         => esc_html__( 'Tasks', 'planet4-blocks' ),
-
-				/*
-				 * Include an icon with your shortcode. Optional.
-				 * Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
-				 */
 				'listItemImage' => 'dashicons-screenoptions',
-
-				/*
-				 * Define the UI for attributes of the shortcode. Optional.
-				 *
-				 * See above, to where the the assignment to the $fields variable was made.
-				 */
 				'attrs'         => $fields,
 			);
 
@@ -115,43 +100,43 @@ if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 		 * Callback for the tasks shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array $attr
+		 * @param array  $attributes  Defined attributes array for this shortcode.
 		 * @param string $content
-		 * @param string $shortcode_tag
+		 * @param string $shortcode_tag Shortcode tag name.
 		 *
 		 * @return string
 		 */
-		public function prepare_template( $attr, $content, $shortcode_tag ) : string {
+		public function prepare_template( $attributes, $content, $shortcode_tag ) : string {
 
-			$attrs_temp = [];
+			$attributes_temp = [];
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp       = [
-					"title_$i"       => esc_html( $attr["title_$i"] ),
-					"description_$i" => wpautop( esc_html( $attr["description_$i"] ) ),
-					"attachment_$i"  => $attr["attachment_$i"],
-					"button_text_$i" => esc_html( $attr["button_text_$i"] ),
-					"button_link_$i" => esc_html( $attr["button_link_$i"] ),
+				$temp_array       = [
+					"title_$i"       => $attributes[ "title_$i" ],
+					"description_$i" => wpautop( $attributes[ "description_$i" ] ),
+					"attachment_$i"  => $attributes[ "attachment_$i" ],
+					"button_text_$i" => $attributes[ "button_text_$i" ],
+					"button_link_$i" => $attributes[ "button_link_$i" ],
 				];
-				$attrs_temp = array_merge( $attrs_temp, $temp );
+				$attributes_temp = array_merge( $attributes_temp, $temp_array );
 			}
-			$attr = shortcode_atts( $attrs_temp, $attr, $shortcode_tag );
+			$attributes = shortcode_atts( $attributes_temp, $attributes, $shortcode_tag );
 
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp = wp_get_attachment_image_src( $attr["attachment_$i"] );
-				if ( $temp !== false && ! empty( $temp ) ) {
-					$attr["attachment_$i"] = $temp[0];
+				$temp_array = wp_get_attachment_image_src( $attributes[ "attachment_$i" ] );
+				if ( false !== $temp_array && ! empty( $temp_array ) ) {
+					$attributes[ "attachment_$i" ] = $temp_array[0];
 				}
 			}
 
-			$data = [
-				'fields'              => $attr,
+			$block_data = [
+				'fields'              => $attributes,
 				'available_languages' => P4BKS_LANGUAGES,
 				'domain'              => 'planet4-blocks',
 			];
 
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
-			$this->view->block( $this->block_name, $data );
+			$this->view->block( $this->block_name, $block_data );
 
 			return ob_get_clean();
 		}
