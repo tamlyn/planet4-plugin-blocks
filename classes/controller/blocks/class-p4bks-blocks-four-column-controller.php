bc67d55a9f3c8552db5755940353e54825ea76c2
Kiriakos Diamantis <replicator31@hotmail.com>
2017-09-27 14:35:16 +0300

    Fixed codesniffer inspection warnings based on on wp standards

--- classes/controller/blocks/class-p4bks-blocks-four-column-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-four-column-controller.php
@@ -4,6 +4,11 @@ namespace P4BKS\Controllers\Blocks;
 
 if ( ! class_exists( 'P4BKS_Blocks_FourColumn_Controller' ) ) {
 
+	/**
+	 * Class P4BKS_Blocks_FourColumn_Controller
+	 *
+	 * @package P4BKS\Controllers\Blocks
+	 */
 	class P4BKS_Blocks_FourColumn_Controller extends P4BKS_Blocks_Controller {
 
 		/**
@@ -25,16 +30,16 @@ if ( ! class_exists( 'P4BKS_Blocks_FourColumn_Controller' ) ) {
 		 */
 		public function prepare_fields() {
 
-			// This block will have 4 different columns with same fields
+			// This block will have 4 different columns with same fields.
 			$fields = [];
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$ordinal = $i . date( "S", mktime( 0, 0, 0, 0, $i, 0 ) );
+				$ordinal = $i . date( 'S', mktime( 0, 0, 0, 0, $i, 0 ) );
 				$field   = [
 					[
 						'label'       => esc_html__( "Select Image for $ordinal column", 'planet4-blocks' ),
 						'attr'        => 'attachment_' . $i,
 						'type'        => 'attachment',
-						'libraryType' => array( 'image' ),
+						'libraryType' => [ 'image' ],
 						'addButton'   => esc_html__( 'Select Image', 'shortcode-ui' ),
 						'frameTitle'  => esc_html__( 'Select Image', 'shortcode-ui' ),
 					],
@@ -69,28 +74,12 @@ if ( ! class_exists( 'P4BKS_Blocks_FourColumn_Controller' ) ) {
 				$fields  = array_merge( $fields, $field );
 			}
 
-			/*
-			 * Define the Shortcode UI arguments.
-			 */
-			$shortcode_ui_args = array(
-				/*
-				 * How the shortcode should be labeled in the UI. Required argument.
-				 */
+			// Define the Shortcode UI arguments.
+			$shortcode_ui_args = [
 				'label'         => esc_html__( 'Content Four Column', 'planet4-blocks' ),
-
-				/*
-				 * Include an icon with your shortcode. Optional.
-				 * Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
-				 */
 				'listItemImage' => 'dashicons-grid-view',
-
-				/*
-				 * Define the UI for attributes of the shortcode. Optional.
-				 *
-				 * See above, to where the the assignment to the $fields variable was made.
-				 */
 				'attrs'         => $fields,
-			);
+			];
 
 			shortcode_ui_register_for_shortcode( 'shortcake_' . $this->block_name, $shortcode_ui_args );
 		}
@@ -99,42 +88,42 @@ if ( ! class_exists( 'P4BKS_Blocks_FourColumn_Controller' ) ) {
 		 * Callback for the four column shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array $attr
+		 * @param array $attributes
 		 * @param string $content
 		 * @param string $shortcode_tag
 		 *
 		 * @return string
 		 */
-		public function prepare_template( $attr, $content, $shortcode_tag ): string {
+		public function prepare_template( $attributes, $content, $shortcode_tag ) : string {
 
-			$attrs_temp = [];
+			$attributes_temp = [];
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp       = [
-					"title_$i"       => esc_html( $attr["title_$i"] ),
-					"description_$i" => wpautop( esc_html( $attr["description_$i"] ) ),
-					"attachment_$i"  => $attr["attachment_$i"],
-					"image_link_$i" => esc_html( $attr["image_link_$i"] ),
+				$temp = [
+					"title_$i"       => __( $attributes[ "title_$i" ] ),
+					"description_$i" => wpautop( __( $attributes[ "description_$i" ] ) ),
+					"attachment_$i"  => __( $attributes[ "attachment_$i" ] ),
+					"image_link_$i"  => $attributes[ "image_link_$i" ],
 				];
-				$attrs_temp = array_merge( $attrs_temp, $temp );
+				$attributes_temp = array_merge( $attributes_temp, $temp );
 			}
-			$attr = shortcode_atts( $attrs_temp, $attr, $shortcode_tag );
+			$attributes = shortcode_atts( $attributes_temp, $attributes, $shortcode_tag );
 
 			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp = wp_get_attachment_image_src( $attr["attachment_$i"] );
-				if ( $temp !== false && ! empty( $temp ) ) {
-					$attr["attachment_$i"] = $temp[0];
+				$temp = wp_get_attachment_image_src( $attributes[ "attachment_$i" ] );
+				if ( false !== $temp && ! empty( $temp ) ) {
+					$attributes[ "attachment_$i" ] = $temp[0];
 				}
 			}
 
-			$data = [
-				'fields'              => $attr,
+			$block_data = [
+				'fields'              => $attributes,
 				'available_languages' => P4BKS_LANGUAGES,
 				'domain'              => 'planet4-blocks',
 			];
 
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
-			$this->view->block( $this->block_name, $data );
+			$this->view->block( $this->block_name, $block_data );
 
 			return ob_get_clean();
 		}
