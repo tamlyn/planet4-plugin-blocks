90bd8143ea8df80b2202983a2aebe629b63dafc7
Kiriakos Diamantis <replicator31@hotmail.com>
2017-10-02 16:28:18 +0300

    Changed carousel split to use wordpress media library to pass variable number of images

--- classes/controller/blocks/class-p4bks-blocks-carouselsplit-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-carouselsplit-controller.php
@@ -30,83 +30,63 @@ if ( ! class_exists( 'P4BKS_Blocks_CarouselSplit_Controller' ) ) {
 		public function prepare_fields() {
 
 			// This block will have 4 different columns with same fields.
-			$fields = [];
-			for ( $i = 1; $i < 5; $i ++ ) {
-				$ordinal = $i . date( 'S', mktime( 0, 0, 0, 0, $i, 0 ) );
-				$field   = [
-					[
-						'label' => esc_html__( 'Title', 'planet4-blocks' ),
-						'attr'  => 'title_' . $i,
-						'type'  => 'text',
-						'meta'  => [
-							'placeholder' => esc_html__( "Enter title of $ordinal image", 'planet4-blocks' ),
-							'data-plugin' => 'planet4-blocks',
-						],
-					],
-					[
-						'label' => esc_html__( 'Caption', 'planet4-blocks' ),
-						'attr'  => 'caption_' . $i,
-						'type'  => 'textarea',
-						'meta'  => [
-							'placeholder' => esc_html__( "Enter caption of $ordinal image", 'planet4-blocks' ),
-							'data-plugin' => 'planet4-blocks',
-						],
-					],
-					[
-						'label'       => esc_html__( 'Attachment', 'shortcode-ui' ),
-						'attr'        => 'attachment_' . $i,
-						'type'        => 'attachment',
-						'libraryType' => [ 'image' ],
-						'addButton'   => esc_html__( 'Select Image', 'shortcode-ui' ),
-						'frameTitle'  => esc_html__( 'Select Image', 'shortcode-ui' ),
-					],
-				];
-				$fields  = array_merge( $fields, $field );
-			}
+			$fields = [
+				[
+					'label'       => __( 'Images', 'shortcode-ui' ),
+					'attr'        => 'multiple_images',
+					'type'        => 'attachment',
+					'libraryType' => [ 'image' ],
+					'multiple'    => true,
+					'addButton'   => __( 'Select Images for Carousel', 'shortcode-ui' ),
+					'frameTitle'  => __( 'Select Images for Carousel', 'shortcode-ui' ),
+				],
+			];
+
 			// Define the Shortcode UI arguments.
-			$shortcode_ui_args = array(
-				'label'         => esc_html__( 'Carousel Split', 'planet4-blocks' ),
-				'listItemImage' => 'dashicons-grid-view',
+			$shortcode_ui_args = [
+				// translators: A block that contains a carousel with split images.
+				'label'         => __( 'Carousel Split', 'planet4-blocks' ),
+				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-plugin-blocks/icons/carousel_split.png' ) . '" />',
 				'attrs'         => $fields,
-			);
+			];
+
 			shortcode_ui_register_for_shortcode( 'shortcake_' . $this->block_name, $shortcode_ui_args );
 		}
 
 		/**
-		 * Callback for the shortcode.
+		 * Callback for the carousel split shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array  $attrs Fields array defined for this shortcode.
-		 * @param string $content
-		 * @param string $shortcode_tag
+		 * @param array  $attributes    Defined attributes array for this shortcode.
+		 * @param string $content       Content.
+		 * @param string $shortcode_tag Shortcode tag name.
 		 *
-		 * @since 0.1.0
-		 *
-		 * @return string
+		 * @return string Returns the compiled template.
 		 */
-		public function prepare_template( $attrs, $content, $shortcode_tag ) : string {
+		public function prepare_template( $attributes, $content, $shortcode_tag ) : string {
 
-			$attrs_temp = [];
-			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp_array       = [
-					"title_$i"       => $attrs[ "title_$i" ],
-					"caption_$i"     => wpautop( esc_html( $attrs[ "caption_$i" ] ) ),
-					"attachment_$i"  => $attrs[ "attachment_$i" ],
-				];
-				$attrs_temp = array_merge( $attrs_temp, $temp_array );
-			}
-			$attrs = shortcode_atts( $attrs_temp, $attrs, $shortcode_tag );
-			for ( $i = 1; $i < 5; $i ++ ) {
-				$temp_array = wp_get_attachment_image_src( $attrs[ "attachment_$i" ] );
+			$images = [];
+			$images_ids = explode( ',',$attributes['multiple_images'] );
+			foreach ( $images_ids as $image_id ) {
+
+				$temp_array = wp_get_attachment_image_src( $image_id );
 				if ( false !== $temp_array && ! empty( $temp_array ) ) {
-					$attrs[ "attachment_$i" ] = $temp_array[0];
+
+					$temp_image = wp_prepare_attachment_for_js( $image_id );
+					$image = [];
+					$image[0] = $temp_image['url'];
+					$image[1] = $temp_image['title'];
+					$image[2] = $temp_image['caption'];
+					$images[] = $image;
 				}
 			}
+
 			$block_data = [
-				'fields'              => $attrs,
+				'fields'              => $images,
 				'available_languages' => P4BKS_LANGUAGES,
 				'domain'              => 'planet4-blocks',
 			];
+
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
 			$this->view->block( $this->block_name, $block_data );
