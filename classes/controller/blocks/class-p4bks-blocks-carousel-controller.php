0ec9d2b2bde20400744d0031625c93a212506814
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-10-03 18:35:52 +0530

    PLANET-1267 Added a custom meta fields for storing image credit text

--- classes/controller/blocks/class-p4bks-blocks-carousel-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-carousel-controller.php
@@ -12,8 +12,51 @@ if ( ! class_exists( 'P4BKS_Blocks_Carousel_Controller' ) ) {
 		 */
 		public function load() {
 			// --- Set here the name of your block ---
+			add_filter( 'attachment_fields_to_edit', array( $this, 'add_image_attachment_fields_to_edit' ), null, 2 );
+			add_filter( 'attachment_fields_to_save', array( $this, 'add_image_attachment_fields_to_save' ), null , 2 );
 			$this->block_name = 'carousel';
 			parent::load();
+		}
+
+		/**
+		 * Add custom media metadata fields
+		 *
+		 * Be sure to sanitize your data before saving it
+		 * http://codex.wordpress.org/Data_Validation
+		 *
+		 * @param $form_fields An array of fields included in the attachment form
+		 * @param $post The attachment record in the database
+		 * @return $form_fields The final array of form fields to use
+		 */
+		function add_image_attachment_fields_to_edit( $form_fields, $post ) {
+
+			// Add a Credit field
+			$form_fields['credit_text'] = array(
+				'label' => __( 'Credit' ),
+				'input' => 'text', // this is default if "input" is omitted
+				'value' => esc_attr( get_post_meta( $post->ID, '_credit_text', true ) ),
+				'helps' => __( 'The owner of the image.' ),
+			);
+
+			return $form_fields;
+		}
+
+		/**
+		 * Save custom media metadata fields
+		 *
+		 * Be sure to validate your data before saving it
+		 * http://codex.wordpress.org/Data_Validation
+		 *
+		 * @param $post The $post data for the attachment
+		 * @param $attachment The $attachment part of the form $_POST ($_POST[attachments][postID])
+		 * @return $post
+		 */
+		function add_image_attachment_fields_to_save( $post, $attachment ) {
+			if ( isset( $attachment['credit_text'] ) ) {
+				update_post_meta( $post['ID'], '_credit_text', esc_attr( $attachment['credit_text'] ) );
+			}
+
+			return $post;
 		}
 
 		/**
@@ -95,13 +138,26 @@ if ( ! class_exists( 'P4BKS_Blocks_Carousel_Controller' ) ) {
 		 */
 		public function prepare_template( $fields, $content, $shortcode_tag ) : string {
 
-			$explodeMultipleImageArray = explode(',',$fields['multiple_image']);
-			foreach ( $explodeMultipleImageArray as $imageID ) { 
-				$images[] = wp_get_attachment_image_src( $imageID, 'cta' );
+			$explodeMultipleImageArray = explode( ',',$fields['multiple_image'] );
+			$images_data = array();
+
+			foreach ( $explodeMultipleImageArray as $imageID ) {
+
+				$image_data_array = wp_get_attachment_image_src( $imageID, 'cta' );
+
+				$images_data['image_src'] = $image_data_array[0];
+				$image_metadata = get_post( $imageID );
+				$attachment_fields = get_post_custom( $imageID );
+
+				$images_data['credits'] = ( isset( $attachment_fields['_credit_text'][0] ) && ! empty( $attachment_fields['_credit_text'][0] ) ) ? esc_attr( $attachment_fields['_credit_text'][0] ) : '';
+
+				$images_data['caption'] = $image_metadata->post_excerpt;
+
+				$images[] = $images_data;
 			}
 
 			$data = [
-				'images' => array_map( 'wp_kses_post', $images),
+				'images' => array_map( 'wp_kses_post', $images ),
 			];
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
