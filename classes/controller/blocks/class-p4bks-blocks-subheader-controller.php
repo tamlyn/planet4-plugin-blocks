7095202ae1688e1e6c9eb4baed0713ea7665bb9d
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2017-10-04 13:02:25 +0300

    Moved icon from /icons/ directory to already existing /admin/images/ directory

--- classes/controller/blocks/class-p4bks-blocks-subheader-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-subheader-controller.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'P4BKS_Blocks_Subheader_Controller' ) ) {
 			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = array(
 				'label'         => __( 'Subheader', 'planet4-blocks' ),
-				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-plugin-blocks/icons/subheader.png' ) . '" />',
+				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-plugin-blocks/admin/images/icon_subheader.png' ) . '" />',
 				'attrs'         => $fields,
 			);
 
