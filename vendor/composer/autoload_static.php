99e1d66c1fc3e2dea89f673c3884735c247c98fd
Kiriakos Diamantis <replicator31@hotmail.com>
2017-10-04 12:19:41 +0300

    Added composer autoload classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -37,6 +37,7 @@ class ComposerStaticInit356100170ff2882375cd04c4c76f9df3
 
     public static $classMap = array (
         'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_Controller' => __DIR__ . '/../..' . '/classes/controller/blocks/class-p4bks-blocks-controller.php',
+        'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_FourColumn_Controller' => __DIR__ . '/../..' . '/classes/controller/blocks/class-p4bks-blocks-four-column-controller.php',
         'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_HappyPoint_Controller' => __DIR__ . '/../..' . '/classes/controller/blocks/class-p4bks-blocks-happypoint-controller.php',
         'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_TwoColumn_Controller' => __DIR__ . '/../..' . '/classes/controller/blocks/class-p4bks-blocks-twocolumn-controller.php',
         'P4BKS\\Controllers\\Menu\\P4BKS_Controller' => __DIR__ . '/../..' . '/classes/controller/menu/class-p4bks-controller.php',
