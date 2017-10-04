dfa19415e2797189907bb6a321529699b47354f6
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-10-04 12:33:08 +0530

    PLANET-1300 Minor code fixes

--- classes/controller/blocks/class-p4bks-blocks-articles-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-articles-controller.php
@@ -11,40 +11,19 @@ if ( ! class_exists( 'P4BKS_Blocks_Articles_Controller' ) ) {
 		 * Override this method in order to give your block its own name.
 		 */
 		public function load() {
-			// --- Set here the name of your block ---
 			$this->block_name = 'articles';
 			parent::load();
 		}
 
 		/**
-		 * Shortcode UI setup for the twocolumn shortcode.
+		 * Shortcode UI setup for the article shortcode.
 		 *
 		 * It is called when the Shortcake action hook `register_shortcode_ui` is called.
 		 *
 		 * @since 0.1.0
 		 */
 		public function prepare_fields() {
-			/*
-			 * Define the UI for attributes of the shortcode. Optional.
-			 *
-			 * If no UI is registered for an attribute, then the attribute will
-			 * not be editable through Shortcake's UI. However, the value of any
-			 * unregistered attributes will be preserved when editing.
-			 *
-			 * Each array must include 'attr', 'type', and 'label'.
-			 * * 'attr' should be the name of the attribute.
-			 * * 'type' options include: text, checkbox, textarea, radio, select, email,
-			 *     url, number, and date, post_select, attachment, color.
-			 * * 'label' is the label text associated with that input field.
-			 *
-			 * Use 'meta' to add arbitrary attributes to the HTML of the field.
-			 *
-			 * Use 'encode' to encode attribute data. Requires customization in shortcode callback to decode.
-			 *
-			 * Depending on 'type', additional arguments may be available.
-			 */
 				$fields = array(
-
 					array(
 						'label' => esc_html__( 'Article Heading', 'planet4-blocks' ),
 						'attr'  => 'article_heading',
@@ -53,7 +32,6 @@ if ( ! class_exists( 'P4BKS_Blocks_Articles_Controller' ) ) {
 							'placeholder' => esc_html__( 'Enter article heading', 'planet4-blocks' ),
 						),
 					),
-
 					array(
 						'label' => esc_html__( 'Article Count', 'planet4-blocks' ),
 						'attr'  => 'article_count',
@@ -64,25 +42,10 @@ if ( ! class_exists( 'P4BKS_Blocks_Articles_Controller' ) ) {
 					),
 				);
 
-			/*
-			 * Define the Shortcode UI arguments.
-			 */
+			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = array(
-				/*
-				 * How the shortcode should be labeled in the UI. Required argument.
-				 */
 				'label' => esc_html__( 'Articles', 'planet4-blocks' ),
-
-				/*
-				 * Include an icon with your shortcode. Optional.
-				 * Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
-				 */
 				'listItemImage' => 'dashicons-editor-table',
-
-				/*
-				 * Define the UI for attributes of the shortcode. Optional.
-				 * See above, to where the the assignment to the $fields variable was made.
-				 */
 				'attrs' => $fields,
 			);
 
@@ -125,6 +88,7 @@ if ( ! class_exists( 'P4BKS_Blocks_Articles_Controller' ) ) {
 				'fields' => array_map( 'wp_kses_post', $fields ),
 				'recent_posts'  => $recent_posts,
 			];
+
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
 			$this->view->block( $this->block_name, $data );
