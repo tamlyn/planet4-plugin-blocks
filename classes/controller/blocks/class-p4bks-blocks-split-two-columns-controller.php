bb9eebec7f44937c192ca185d21170b867728fb2
Angelos Theodorakopoulos <angtheod@gmail.com>
2017-10-10 16:45:42 +0300

    Feature/1310 Block: Split Two Columns.

---
+++ classes/controller/blocks/class-p4bks-blocks-split-two-columns-controller.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace P4BKS\Controllers\Blocks;
+
+if ( ! class_exists( 'P4BKS_Blocks_Split_Two_Columns_Controller' ) ) {
+
+	class P4BKS_Blocks_Split_Two_Columns_Controller extends P4BKS_Blocks_Controller {
+
+
+		/**
+		 * Override this method in order to give your block its own name.
+		 */
+		public function load() {
+			$this->block_name = 'split_two_columns';
+			parent::load();
+		}
+
+		/**
+		 * Shortcode UI setup for the shortcode.
+		 * It is called when the Shortcake action hook `register_shortcode_ui` is called.
+		 */
+		public function prepare_fields() {
+			$fields = [
+				[
+					'label' => __( 'Title', 'planet4-blocks' ),
+					'attr'  => 'title',
+					'type'  => 'text',
+					'meta'  => [
+						'placeholder' => __( 'Enter title', 'planet4-blocks' ),
+						'data-plugin' => 'planet4-blocks',
+					],
+				],
+			];
+
+			/*
+			 * Define the Shortcode UI arguments.
+			 */
+			$shortcode_ui_args = [
+				/*
+				 * How the shortcode should be labeled in the UI. Required argument.
+				 */
+				'label' => __( 'Split two columns', 'planet4-blocks' ),
+
+				/*
+				 * Include an icon with your shortcode. Optional.
+				 * Use a dashicon, or full HTML (e.g. <img src="/path/to/your/icon" />).
+				 */
+				'listItemImage' => '<img src="' . esc_url( plugins_url() . "/planet4-plugin-blocks/admin/images/$this->block_name.png" ) . '" />',
+
+				/*
+				 * Define the UI for attributes of the shortcode. Optional.
+				 * See above, to where the the assignment to the $fields variable was made.
+				 */
+				'attrs' => $fields,
+			];
+
+			shortcode_ui_register_for_shortcode( 'shortcake_' . $this->block_name, $shortcode_ui_args );
+		}
+
+		/**
+		 * Callback for the shortcode.
+		 * It renders the shortcode based on supplied attributes.
+		 *
+		 * @param array  $fields Array of fields that are to be used in the template.
+		 * @param string $content The content of the post.
+		 * @param string $shortcode_tag The shortcode tag.
+		 *
+		 * @return string
+		 */
+		public function prepare_template( $fields, $content, $shortcode_tag ) : string {
+			$data = [
+				'fields' => $fields,
+			];
+			// Shortcode callbacks must return content, hence, output buffering here.
+			ob_start();
+			$this->view->block( $this->block_name, $data );
+
+			return ob_get_clean();
+		}
+	}
+}
