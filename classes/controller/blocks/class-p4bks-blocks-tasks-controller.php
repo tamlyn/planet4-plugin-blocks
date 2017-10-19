f883bb1f9dfb65fde945bb80f7889b68b2681391
Kiriakos Diamantis <replicator31@hotmail.com>
2017-10-19 12:23:03 +0300

    Reverted translation strings. Removed array merge

--- classes/controller/blocks/class-p4bks-blocks-tasks-controller.php
+++ classes/controller/blocks/class-p4bks-blocks-tasks-controller.php
@@ -53,53 +53,69 @@ if ( ! class_exists( 'P4BKS_Blocks_Tasks_Controller' ) ) {
 
 			// This block will have 4 different columns with same fields.
 			for ( $i = 1; $i < 5; $i++ ) {
-				$field = [
+
+				$fields[] =
 					[
-						'label' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Title', 'planet4-blocks' ),
+						// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+						'label' => sprintf( __( 'Task %s: Title', 'planet4-blocks' ), $i ),
 						'attr'  => 'title_' . $i,
 						'type'  => 'text',
 						'meta'  => [
-							'placeholder' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Enter Title', 'planet4-blocks' ),
+							// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+							'placeholder' => sprintf( __( 'Enter title of %s task/column', 'planet4-blocks' ), $i ),
 							'data-plugin' => 'planet4-blocks',
 						],
-					],
+					];
+
+				$fields[] =
 					[
-						'label' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Description', 'planet4-blocks' ),
+						// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+						'label' => sprintf( __( 'Task %s: Description', 'planet4-blocks' ), $i ),
 						'attr'  => 'description_' . $i,
 						'type'  => 'textarea',
 						'meta'  => [
-							'placeholder' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Enter Description', 'planet4-blocks' ),
+							// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+							'placeholder' => sprintf( __( 'Enter description of %s task/column', 'planet4-blocks' ), $i ),
 							'data-plugin' => 'planet4-blocks',
 						],
-					],
+					];
+
+				$fields[] =
 					[
-						'label'       => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Select Image', 'shortcode-ui' ),
+						// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+						'label'       => sprintf( __( 'Select Image for %s task/column', 'planet4-blocks' ), $i ),
 						'attr'        => 'attachment_' . $i,
 						'type'        => 'attachment',
 						'libraryType' => [ 'image' ],
 						'addButton'   => __( 'Select Image', 'shortcode-ui' ),
 						'frameTitle'  => __( 'Select Image', 'shortcode-ui' ),
-					],
+					];
+
+				$fields[] =
 					[
-						'label' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Button Text', 'planet4-blocks' ),
+						// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+						'label' => sprintf( __( 'Task %s: Button Text', 'planet4-blocks' ), $i ),
 						'attr'  => 'button_text_' . $i,
 						'type'  => 'text',
 						'meta'  => [
-							'placeholder' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Enter Button Text', 'planet4-blocks' ),
+							// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+							'placeholder' => sprintf( __( 'Enter button text of %s task/column', 'planet4-blocks' ), $i ),
 							'data-plugin' => 'planet4-blocks',
 						],
-					],
+					];
+
+				$fields[] =
 					[
-						'label' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Button Link', 'planet4-blocks' ),
+						// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+						'label' => sprintf( __( 'Task %s: Button Link', 'planet4-blocks' ), $i ),
 						'attr'  => 'button_link_' . $i,
 						'type'  => 'url',
 						'meta'  => [
-							'placeholder' => __( 'Task', 'planet4-blocks' ) . ' ' . $i . ': ' . __( 'Enter Button Link', 'planet4-blocks' ),
+							// translators: placeholder needs to represent the ordinal of the task/column, eg. 1st, 2nd etc.
+							'placeholder' => sprintf( __( 'Enter button link of %s task/column', 'planet4-blocks' ), $i ),
 							'data-plugin' => 'planet4-blocks',
 						],
-					],
-				];
-				$fields = array_merge( $fields, $field );
+					];
 			}
 
 			// Define the Shortcode UI arguments.
