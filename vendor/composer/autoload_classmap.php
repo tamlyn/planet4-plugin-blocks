a50089eb32a1d3ca1a7784e91d4691184a65bf43
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-10-25 19:53:46 +0530

    Autoload files

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -11,6 +11,7 @@ return array(
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_CarouselSplit_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-carouselsplit-controller.php',
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_Carousel_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-carousel-controller.php',
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_ContentFourColumn_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-contentfourcolumn-controller.php',
+    'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_ContentThreeColumn_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-contentthreecolumn-controller.php',
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-controller.php',
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_Covers_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-covers-controllers.php',
     'P4BKS\\Controllers\\Blocks\\P4BKS_Blocks_HappyPoint_Controller' => $baseDir . '/classes/controller/blocks/class-p4bks-blocks-happypoint-controller.php',
