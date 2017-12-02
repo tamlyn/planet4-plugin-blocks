3c2b71ce12d39e7e83f20c582325a63619a36afd
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-12-02 12:05:50 +0530

    Settings reference changes in happy point block

--- classes/controller/blocks/class-happypoint-controller.php
+++ classes/controller/blocks/class-happypoint-controller.php
@@ -79,7 +79,7 @@ if ( ! class_exists( 'HappyPoint_Controller' ) ) {
 
 			$fields['background_html']     = wp_get_attachment_image( $fields['background'] );
 			$fields['background_src']      = wp_get_attachment_image_src( $fields['background'], 'full' );
-			$fields['engaging_network_id'] = get_option( 'engaging_network_form_id', '' ) ? get_option( 'engaging_network_form_id' ) : '';
+			$fields['engaging_network_id'] = planet4_get_option( 'engaging_network_form_id' ) ?? '';
 			$fields['opacity']             = $opacity;
 
 			$data = [
