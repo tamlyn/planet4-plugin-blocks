a723a3ae3d817fc68bdf504c92c0af7b152b5a38
Ray Walker <geek@raywalker.it>
2017-12-15 15:52:41 +1100

    Generate srcset with wp_calculate_image_srcset().

--- classes/controller/blocks/class-carouselheader-controller.php
+++ classes/controller/blocks/class-carouselheader-controller.php
@@ -154,6 +154,7 @@ if ( ! class_exists( 'CarouselHeader_Controller' ) ) {
 				$temp_array = wp_get_attachment_image_src( $image_id, 'large' );
 				if ( false !== $temp_array && ! empty( $temp_array ) ) {
 					$attributes[ "image_$i" ] = $temp_array[0];
+					$attributes[ "image_${i}_srcset" ] = wp_calculate_image_srcset(['1024', '768'], wp_get_attachment_image_src( $image_id, 'full' )[0], wp_get_attachment_metadata( $image_id ));
 					$total_images++;
 				}
 				$temp_image                     = wp_prepare_attachment_for_js( $image_id );
