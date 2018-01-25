d88f5d9de4d37995695532526b72e3fcfdb78152
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-01-25 16:13:26 +0530

    Added typecasting

--- classes/controller/blocks/class-articles-controller.php
+++ classes/controller/blocks/class-articles-controller.php
@@ -85,7 +85,7 @@ if ( ! class_exists( 'Articles_Controller' ) ) {
 			$fields['article_count']  = ( ! empty( $fields['article_count'] ) ) ? $fields['article_count'] : 3;
 			$fields['read_more_text'] = $fields['read_more_text'] ?? __( 'READ ALL THE NEWS', 'planet4-blocks' );
 			$fields['read_more_link'] = ( ! empty( $fields['read_more_link'] ) ) ? $fields['read_more_link'] : '/?s=&orderby=post_date&f[ctype][Post]=3';
-			$exclude_post_id          = $fields['post_id'] ?? '';
+			$exclude_post_id          = (int) $fields['exclude_post_id'] ?? '';
 
 			//Get page categories
 			$post_categories         = get_the_category();
