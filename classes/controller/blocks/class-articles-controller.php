7765adc21b46b0a79df50e224fdb590e554779bf
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-01-31 15:47:14 +0200

    Use the correct domain/site_url for the all_articles link

--- classes/controller/blocks/class-articles-controller.php
+++ classes/controller/blocks/class-articles-controller.php
@@ -84,7 +84,7 @@ if ( ! class_exists( 'Articles_Controller' ) ) {
 			// Read more button links to search results if no link is specified.
 			$fields['article_count']  = ( ! empty( $fields['article_count'] ) ) ? $fields['article_count'] : 3;
 			$fields['read_more_text'] = $fields['read_more_text'] ?? __( 'READ ALL THE NEWS', 'planet4-blocks' );
-			$fields['read_more_link'] = ( ! empty( $fields['read_more_link'] ) ) ? $fields['read_more_link'] : '/?s=&orderby=post_date&f[ctype][Post]=3';
+			$fields['read_more_link'] = ( ! empty( $fields['read_more_link'] ) ) ? $fields['read_more_link'] : get_site_url() . '/?s=&orderby=post_date&f[ctype][Post]=3';
 			$exclude_post_id          = (int) $fields['exclude_post_id'] ?? '';
 			$tag_id                   = $fields['tag_id'] ?? '';
 
