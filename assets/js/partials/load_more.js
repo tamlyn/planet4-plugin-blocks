ae36fd02ea2cc0043f7eb0b50d99e06084b371f1
Pablo Cubico <pablocuadrado@gmail.com>
2018-11-27 09:37:46 -0300

    Prevent focus on Load More buttons in all blocks.

--- assets/js/partials/load_more.js
+++ assets/js/partials/load_more.js
@@ -13,11 +13,12 @@ $(document).ready(function () {
   });
 
   // Add click event for load more button in Covers blocks.
-  $('.btn-load-more-posts-click').off('click').on('click', function () {
+  $('.btn-load-more-posts-click').off('mousedown touchstart').on('mousedown touchstart', function (e) {
+    e.preventDefault();
+
     var $row = $('.post-column:hidden', $(this).closest('.container'));
     var posts_per_row = $(this).closest('.four-column-content').data('posts_per_row');
 
-    $(this).blur();
     if ($row.length > 0) {
       $row.slice( 0, posts_per_row ).show('slow');
     }
@@ -38,11 +39,12 @@ $(document).ready(function () {
   });
 
   // Add click event for load more button in Covers blocks.
-  $('.btn-load-more-covers-click').off('click').on('click', function () {
+  $('.btn-load-more-covers-click').off('mousedown touchstart').on('mousedown touchstart', function (e) {
+    e.preventDefault();
+
     var $row = $('.cover-card-column:hidden', $(this).closest('.container'));
     var covers_per_row = $(this).closest('.covers-block').data('covers_per_row');
 
-    $(this).blur();
     if ($row.length > 0) {
       $row.slice( 0, covers_per_row ).show('slow');
     }
@@ -52,11 +54,12 @@ $(document).ready(function () {
   });
 
   // Add click event for load more button in Articles blocks.
-  $('.btn-load-more-articles-click').off('click').on('click', function () {
+  $('.btn-load-more-articles-click').off('mousedown touchstart').on('mousedown touchstart', function (e) {
+    e.preventDefault();
+
     var $articles = $('.article-list-item.d-none', $(this).closest('.container'));
     var articles_per_click = 3;
 
-    $(this).blur();
     if ($articles.length > 0) {
 
       // Lazy-load images of hidden articles.
@@ -76,7 +79,7 @@ $(document).ready(function () {
     }
   });
 
-  $('.load-more').off('click').on('click', function(e) {
+  $('.load-more').off('mousedown touchstart').on('mousedown touchstart', function(e) {
     e.preventDefault();
 
     // Append response only to current block.
@@ -98,21 +101,21 @@ $(document).ready(function () {
     }).done(function ( response ) {
       // Append the response at the bottom of the results and then show it.
       $content.append( response );
+      if (next_page === total_pages) {
+        $(this).fadeOut();
+      }  
     }).fail(function ( jqXHR, textStatus, errorThrown ) {
       console.log(errorThrown); //eslint-disable-line no-console
     });
-
-    if (next_page === total_pages) {
-      $(this).fadeOut();
-    }
   });
 
   // Add click event handler for load more button in Campaign thumbnail blocks.
-  $('.btn-load-more-campaigns-click').off('click').on('click', function () {
+  $('.btn-load-more-campaigns-click').off('mousedown touchstart').on('mousedown touchstart', function (e) {
+    e.preventDefault();
+
     var $row = $('.campaign-card-column:hidden', $(this).closest('.container'));
     var covers_per_row = 3;
 
-    $(this).blur();
     if ($row.length > 0) {
       $row.slice( 0, covers_per_row ).show('slow');
     }
